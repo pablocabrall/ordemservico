9db503f9dbf494f18d7148d57a9a62fb9b381068
Pablo <opablocabral@gmail.com>
2024-06-25 17:33:53 -0300

    Criação dos arquivos de templates da ordem de serviço

---
+++ backend/servico/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path
+from backend.servico import views as v
+app_name = 'core'
+
+urlpatterns = [
+    path('', v.ordem_servico_list, name='ordem_servico_list'),
+    path('create/', v.ordem_servico_create, name='ordem_servico_create'),
+    path('<int:pk>/', v.ordem_servico_detail, name='ordem_servico_detail'),
+    path('<int:pk>/update/', v.ordem_servico_create, name='ordem_servico_update'),
+
+
+]

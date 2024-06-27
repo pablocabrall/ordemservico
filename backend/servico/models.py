23f81466dfb57580f766227e69dd14216a4a390d
Pablo <opablocabral@gmail.com>
2024-06-27 15:33:22 -0300

    Criação de models de Serviço

--- backend/servico/models.py
+++ backend/servico/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from backend.cliente.models import Cliente
 
 class Servico(models.Model):
     titulo = models.CharField('Titulo', max_length=100)
@@ -10,4 +11,57 @@ class Servico(models.Model):
     
     def __str__(self):
         return f'{self.titulo}'
+    
+SITUACAO = (
+    ('pe', 'Pendente'),
+    ('ca', 'Cancelado'),
+    ('an', 'Andamento'),
+    ('ap', 'Aprovado')
+)
 
+
+class OrdemServico(models.Model):
+    situacao = models.CharField('Situação', max_length=2, choices=SITUACAO)
+    cliente = models.ForeignKey(
+        Cliente,
+        on_delete=models.SET_NULL,
+        verbose_name='cliente',
+        related_name='ordem_servicos',
+        null=True,
+        blank=True
+    )
+
+    class Meta:
+        ordering = ('-pk',)
+        verbose_name = 'ordem de serviço'
+        verbose_name_plural = 'ordens de serviço'
+    
+    def __str__(self):
+        return f'{self.pk}'
+    
+
+class OrdemServicoItem(models.Model):
+    ordem_servico = models.ForeignKey(
+        OrdemServico,
+        on_delete=models.CASCADE,
+        verbose_name='ordem de serviço',
+        related_name = 'ordem_servico_itens',
+    )
+
+    servico = models.ForeignKey(
+        Servico,
+        on_delete=models.CASCADE,
+        verbose_name='serviço',
+        related_name='ordem_servico_item_servicos',
+    )
+
+    valor = models.DecimalField('valor', max_digits=8, decimal_places=2, null=True, blank=True)
+    proxima_visita = models.DateField('Próxima Visita', null=True, blank=True)
+
+    class Meta:
+        ordering = ('-pk',)  # ordem decrescente, se quiser crescente só tirar o sinal de menos.
+        verbose_name = 'item da ordem de serviço'
+        verbose_name_plural = 'itens da ordens de serviço'
+
+    def __str__(self):
+        return f'{self.pk}'
